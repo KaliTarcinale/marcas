ebd04ebd1db745535630e47a439b5bed135e90af
KaliTarcinale <kalitarcinale@gmail.com>
2025-04-10 09:53:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var marca = ["Dior", "Fendi", "Chanel", "Armani", "Versace", "Valentino", "Gucci", "Louis Vuitton", "Burberry", "Prada", "DG", "jacquemus", "Ck", "VS", "Adidas", "Nike"]
+var marcaEscolhida = parseInt(prompt("Digite o número 0 a 15"))
+document.write("Marca Escolhida:" + marca[marcaEscolhida])
